3fc209012388d632da1250d2bdd9432b31795f95
Pine Mizune <pinemz@gmail.com>
2017-02-28 02:08:02 +0900

    feat(delete-old-messages): show debug messages

---
+++ config/default.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  slack: {
+    channel: 'emoji',
+    iconUrl: 'https://dl.dropboxusercontent.com/u/29279948/Slack/Icons/Kanna.png',
+    username: 'kanna',
+    before: '7 days',
+  },
+}

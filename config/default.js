1104be5c431de472dda38b8867d793cc64244dff
Pine Mizune <pinemz@gmail.com>
2017-02-28 03:53:40 +0900

    feat(notify): notify to slack

--- config/default.js
+++ config/default.js
@@ -5,6 +5,7 @@ module.exports = {
     channel: 'emoji',
     iconUrl: 'https://dl.dropboxusercontent.com/u/29279948/Slack/Icons/Kanna.png',
     username: 'kanna',
+    message: '<%= ymd %> 以前のメッセージを <%= count %> 件けしたー',
     search: {
       before: '7 days',
       count: 1000,

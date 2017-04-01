a6b7164309b3053c57c0d07463dc7e71e02fad14
Pine Mizune <pinemz@gmail.com>
2017-04-02 08:12:53 +0900

    fix(git-status): show text diff

--- scripts/test/git-status.sh
+++ scripts/test/git-status.sh
@@ -4,12 +4,12 @@ set -eu
 
 if [ -n "$(git diff --raw)" ]; then
   echo "git repository is dirty"
-  git diff --raw
+  git diff --raw --text
   exit 1
 fi
 
 if [ -n "$(git diff --cached --raw)" ]; then
   echo "git repository is dirty"
-  git diff --cached --raw
+  git diff --cached --raw --text
   exit 1
 fi

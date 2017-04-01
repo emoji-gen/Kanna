9429babd0c50ec9caeba535d0905e4c25c0b0504
Pine Mizune <pinemz@gmail.com>
2017-04-02 08:20:02 +0900

    fix(git-status): ooooops!

--- scripts/test/git-status.sh
+++ scripts/test/git-status.sh
@@ -4,12 +4,12 @@ set -eu
 
 if [ -n "$(git diff --raw)" ]; then
   echo "git repository is dirty"
-  git diff --raw --text
+  git diff
   exit 1
 fi
 
 if [ -n "$(git diff --cached --raw)" ]; then
   echo "git repository is dirty"
-  git diff --cached --raw --text
+  git diff --cached
   exit 1
 fi

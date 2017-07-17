9d22a071f6e3236b0d3afc159a52ff12e297433d
Pine Mizune <pinemz@gmail.com>
2017-07-17 16:58:53 +0900

    fix(config): fix slack icon url

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@
 module.exports = {
   slack: {
     channel: 'emoji',
-    iconUrl: 'https://dl.dropboxusercontent.com/u/29279948/Slack/Icons/Kanna.png',
+    iconUrl: 'https://i.imgur.com/z9ZkgzU.png',
     username: 'kanna',
     message: '<%= ymd %> 以前のメッセージを <%= count %> 件けしたー',
     search: {

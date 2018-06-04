06c21852e092e55cd65dfd80bff4a607375c7f96
Pine Mizune <pinemz@gmail.com>
2018-06-05 01:46:19 +0900

    fix(default.js): update config

--- config/default.js
+++ config/default.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   slack: {
-    notifyChannel: 'emoji',
-    deleteChannels: ['emoji', 'emoji-notice'],
+    notifyChannel: 'dev',
+    deleteChannels: ['dev', 'alert'],
     iconUrl: 'https://i.imgur.com/z9ZkgzU.png',
     username: 'kanna',
     message: '<%= ymd %> 以前のメッセージを <%= count %> 件けしたー',

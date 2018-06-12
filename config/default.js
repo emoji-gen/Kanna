39a909f5afaf9d17df74c49257b82e3e85e1ba01
Pine Mizune <pinemz@gmail.com>
2018-06-12 15:03:43 +0900

    fix(config): fix notification channel

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   slack: {
-    notifyChannel: 'dev',
+    notifyChannel: 'notice',
     deleteChannels: ['dev', 'alert'],
     iconUrl: 'https://i.imgur.com/z9ZkgzU.png',
     username: 'kanna',
